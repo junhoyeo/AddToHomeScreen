b2eadc808a51b35578a77cd6a4ab05a80111341f
Junho Yeo <i@junho.io>
2023-02-23 23:58:59 -0700

    Initial Commit

---
+++ AddToHomeScreen/AddToHomeScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddToHomeScreenApp.swift
+//  AddToHomeScreen
+//
+//  Created by Junho Yeo on 2023/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AddToHomeScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
